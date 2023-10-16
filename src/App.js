4603a77f6bd79f0940ad2d8e61b13f6c0da11436
Erwin Amador <eamador0305@gmail.com>
2023-10-16 14:44:29 -0600

    import navigation.css to navigation.jsx

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import { BrowserRouter as Router } from 'react-router-dom';
 import NavBar from './components/navigation';
 
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <NavBar />
-
-    </Router>
+    <NavBar />
   );
 }
 
--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,5 +1,5 @@
-import { NavLink } from 'react-router-dom';
 import logo from '../assets/logo.png';
+import '../styles/navigation.css';
 
 function NavBar() {
   return (
