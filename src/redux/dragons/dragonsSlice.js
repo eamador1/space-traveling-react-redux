24517067d32f74ad43de843bf76ecdab91326b12
Erwin Amador <eamador0305@gmail.com>
2023-10-19 08:04:59 -0600

    fetch dragon and add initial state in dragonsSlice

--- src/redux/dragons/dragonsSlice.js
+++ src/redux/dragons/dragonsSlice.js
@@ -4,41 +4,24 @@ import axios from 'axios';
 const DRAGON_URL = 'https://api.spacexdata.com/v4/dragons';
 
 export const fetchDragons = createAsyncThunk('dragons/fetchDragons', async () => {
-  try {
-    const { data } = await axios.get(DRAGON_URL);
-    return data.map((dragon) => ({
-      id: dragon.dragon_id,
-      name: dragon.dragon_name,
-      type: dragon.dragon_type,
-      flickr_images: dragon.flickr_images,
-    }));
-  } catch (error) {
-    throw new Error('Error fetching dragons from the API');
-  }
+  const response = await axios.get(DRAGON_URL);
+  return response.data.map((dragon) => ({
+    id: dragon.id,
+    name: dragon.name,
+    type: dragon.type,
+    flickr_images: dragon.flickr_images,
+  }));
 });
 
-const dragonsSlice = createSlice({
+const initialState = [];
+
+export const dragonsSlice = createSlice({
   name: 'dragons',
-  initialState: {
-    dragons: [],
-    status: 'idle',
-    error: null,
-  },
-  reducers: {
-  },
+  initialState,
+  reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(fetchDragons.fulfilled, (state, action) => {
-        state.status = 'succeeded';
-        state.dragons = action.payload;
-      })
-      .addCase(fetchDragons.pending, (state) => {
-        state.status = 'loading';
-      })
-      .addCase(fetchDragons.rejected, (state, action) => {
-        state.status = 'failed';
-        state.error = action.error.message;
-      });
+      .addCase(fetchDragons.fulfilled, (state, action) => action.payload);
   },
 });
 
